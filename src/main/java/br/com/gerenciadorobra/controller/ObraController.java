812191540f20233c5a29dfd54ab8a1671ce240d3
David <David@David-PC.home>
2017-09-19 00:02:27 -0300

    Ajustes Paginacao

--- src/main/java/br/com/gerenciadorobra/controller/ObraController.java
+++ src/main/java/br/com/gerenciadorobra/controller/ObraController.java
@@ -1,5 +1,6 @@
 package br.com.gerenciadorobra.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,9 +26,23 @@ public class ObraController {
 		//obra.setNome("G2O");
 		ModelAndView modelAndView =  new ModelAndView("cadastro/obra");
 		modelAndView.addObject("obra", obra);
-		int pagina = 1;
-		List<Obra> obras  = obraDao.findListaPaginada(pagina);
 		
+		
+		 List<Obra> listaObra = obraDao.findAll();
+		 int numPag = listaObra.size() / 3;
+		
+		 if(numPag <= 0) {
+			 numPag = 1;
+		 }
+		 
+		 ArrayList<Integer> listaPaginacao = new ArrayList<>();
+		 for(int i=0;i <= numPag;i++) {
+			 listaPaginacao.add(i+1); 
+		 }
+		 
+		List<Obra> obras  = obraDao.findListaPaginada(numPag);
+				
+		modelAndView.addObject("listaPaginacao", listaPaginacao);
 		modelAndView.addObject("obras", obras);
 		return modelAndView;
 	}
